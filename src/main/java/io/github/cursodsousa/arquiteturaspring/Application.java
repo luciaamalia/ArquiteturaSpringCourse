5761e0b65c1bc59e6b5261c275c7db8913f17b91
Lúcia Amália <lucia.amaliaof@gmail.com>
2025-03-06 18:10:13 -0300

    Atualização feature
    Criação MailSender e TodoValidator

--- src/main/java/io/github/cursodsousa/arquiteturaspring/Application.java
+++ src/main/java/io/github/cursodsousa/arquiteturaspring/Application.java
@@ -4,12 +4,10 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
-@EnableConfigurationProperties
 public class Application {
 
 	public static void main(String[] args) {
@@ -17,6 +15,21 @@ public class Application {
 
 		SpringApplicationBuilder builder =
 				new SpringApplicationBuilder(Application.class);
+
+		builder.bannerMode(Banner.Mode.OFF);
+		builder.profiles("producao", "homologacao");
+
+		builder.run(args);
+
+
+		// contexto da aplicação já iniciada:
+		ConfigurableApplicationContext applicationContext = builder.context();
+//		var produtoRepository = applicationContext.getBean("produtoRepository");
+
+
+		ConfigurableEnvironment environment = applicationContext.getEnvironment();
+		String applicationName = environment.getProperty("spring.application.name");
+		System.out.println("Nome da aplicação: " + applicationName);
 
 	}
 
