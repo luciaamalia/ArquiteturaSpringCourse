473f7e6cf02f510ecf5c73bbb1edcd6dd1d9cbe4
Lúcia Amália <lucia.amaliaof@gmail.com>
2025-03-06 15:12:30 -0300

    Primeiro commit
    Estudando sobre arquitetura do Framework

---
+++ src/main/java/io/github/cursodsousa/arquiteturaspring/montadora/api/Aspirado.java
@@ -0,0 +1,14 @@
+package io.github.cursodsousa.arquiteturaspring.montadora.api;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME) //anotation esta disponivel em tempo de execucao
+@Target({ElementType.FIELD, ElementType.METHOD}) //a anotattion pode ser aplicada a atributos campos e métodos
+@Qualifier("motorAspirado") //
+public @interface Aspirado {
+}
